5ee3c58a76f6fcb333493e310411ccb7ed839b23
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-02-17 19:17:05 +0000

    Compress car

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ const horizontalSettings: Settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: false,
-  lazyLoad: 'progressive',
 };
 
 const verticalSettings: Settings = {
@@ -75,27 +74,14 @@ const App = () => {
         {...verticalSettings}
         ref={verticalSlider}
         afterChange={handleSlideChange}
-        onSwipe={(event) => console.log(event)}
       >
-        <Slider
-          {...horizontalSettings}
-          ref={firstHorizontalSlider}
-          onSwipe={(event) => console.log(event)}
-        >
+        <Slider {...horizontalSettings} ref={firstHorizontalSlider}>
           {images.map((image) => image)}
         </Slider>
-        <Slider
-          {...horizontalSettings}
-          ref={secondHorizontalSlider}
-          onSwipe={(event) => console.log(event)}
-        >
+        <Slider {...horizontalSettings} ref={secondHorizontalSlider}>
           {images.map((image) => image)}
         </Slider>
-        <Slider
-          {...horizontalSettings}
-          ref={thirdHorizontalSlider}
-          onSwipe={(event) => console.log(event)}
-        >
+        <Slider {...horizontalSettings} ref={thirdHorizontalSlider}>
           {images.map((image) => image)}
         </Slider>
       </Slider>
